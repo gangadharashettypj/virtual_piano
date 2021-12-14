668a53c0e1f82f56fc1471d0f8d028d48916152e
Gangadhara Shetty P J <gangadharashetty.gs@gmail.com>
2021-12-14 16:10:42 +0530

    changed view name

--- app/src/main/java/com/example/pianokeys/MainActivity.kt
+++ app/src/main/java/com/example/pianokeys/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.pianokeys
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.dreamtech.virtual_piano.PianoKey
 
 
 class MainActivity : AppCompatActivity() {
